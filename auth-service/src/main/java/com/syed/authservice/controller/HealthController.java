ce1d3fb9dfc879fe412e84e5ca70b54b0b4c08ca
Abul Syed <syed1234@hotmail.co.uk>
2023-06-28 22:54:44 +0100

    auth-service

---
+++ auth-service/src/main/java/com/syed/authservice/controller/HealthController.java
@@ -0,0 +1,23 @@
+package com.syed.authservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HealthController {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        log.info("Entering HealthController:health");
+
+        return new ResponseEntity<>("Auth service " + appVersion + " running", HttpStatus.OK);
+    }
+}
