3eef257755bc445d20871cd1bda03552acb17e96
Abul Syed <syed1234@hotmail.co.uk>
2023-07-29 12:43:11 +0100

    create-app-form

--- frontend/src/components/form/create-app/CreateAppForm.tsx
+++ frontend/src/components/form/create-app/CreateAppForm.tsx
@@ -6,8 +6,29 @@ interface CreateAppFormInterface {
 }
 
 const CreateAppForm: React.FC<CreateAppFormInterface> = ({ showAppForm, setShowAppForm }) => {
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    console.log('form submitted')
+    setShowAppForm(!showAppForm)
+  }
+
   return (
-    <div>Form</div>
+    <div className='create-app-form-container'>
+      <form className='create-app-form' onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label className='label'>Name<span className="required">*</span></label>
+          <input type="text" required className="form-input" />
+        </div>
+        <div className="form-group">
+          <label className='label'>Description<span className="required">*</span></label>
+          <input type="text" required className="form-input" />
+        </div>
+        <div className='form-group-btn'>
+          <button className='btn btn-primary'>Submit</button>
+          <button className='btn btn-cancel' onClick={() => setShowAppForm(!showAppForm)}>Cancel</button>
+        </div>
+      </form>
+    </div>
   )
 }
 
