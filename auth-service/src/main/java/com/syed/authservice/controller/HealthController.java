71c2ef9126ca3b577dfd29b831ff6f2a3bbd8e5c
Abul Syed <syed1234@hotmail.co.uk>
2023-08-20 19:35:23 +0100

    auth-service gradle v

---
+++ auth-service/src/main/java/com/syed/authservice/controller/HealthController.java
@@ -0,0 +1,19 @@
+package com.syed.authservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health() {
+        return new ResponseEntity<>("Auth service " + appVersion + " running", HttpStatus.OK);
+    }
+}
