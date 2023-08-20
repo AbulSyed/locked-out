14347dd1a71d034478a125c785026ba596160551
Abul Syed <syed1234@hotmail.co.uk>
2023-08-20 19:21:21 +0100

    rm auth-service maven v

--- auth-service/src/main/java/com/syed/authservice/aspect/expression/AopExpressions.java
+++
@@ -1,9 +0,0 @@
-package com.syed.authservice.aspect.expression;
-
-import org.aspectj.lang.annotation.Pointcut;
-
-public class AopExpressions {
-
-    @Pointcut("execution(* com.syed.authservice.controller.*.*(..))")
-    public void forAllControllers() {}
-}
