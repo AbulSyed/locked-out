47f7ca38490a4057ef103badce7ee524302de642
Abul Syed <syed1234@hotmail.co.uk>
2023-09-02 15:26:46 +0100

    integration test setup

---
+++ identity-service/src/integrationTest/java/com/syed/identityservice/AppControllerIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.syed.identityservice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AppControllerIntegrationTest {
+
+    @Test
+    void test() {
+        Assertions.assertEquals(1, 1);
+    }
+}
