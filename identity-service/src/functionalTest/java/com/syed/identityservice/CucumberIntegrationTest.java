a3b52749163b3314f785626510bc0e23946961ca
Abul Syed <syed1234@hotmail.co.uk>
2023-09-03 18:40:32 +0100

    functional test w/ cucumber setup

---
+++ identity-service/src/functionalTest/java/com/syed/identityservice/CucumberIntegrationTest.java
@@ -0,0 +1,11 @@
+package com.syed.identityservice;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features/",
+    glue = { "com.syed.identityservice.cucumberglue" })
+public class CucumberIntegrationTest {
+}
