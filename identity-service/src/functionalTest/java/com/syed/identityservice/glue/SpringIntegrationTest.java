dca16ba8f0dca26bac6f9262ddcef6c187be96b8
Abul Syed <syed1234@hotmail.co.uk>
2023-09-04 17:44:39 +0100

    update FTA to use test containers

--- identity-service/src/functionalTest/java/com/syed/identityservice/glue/SpringIntegrationTest.java
+++ identity-service/src/functionalTest/java/com/syed/identityservice/glue/SpringIntegrationTest.java
@@ -1,9 +1,10 @@
 package com.syed.identityservice.glue;
 
+import com.syed.identityservice.BaseTest;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @CucumberContextConfiguration
-public class SpringIntegrationTest {
+public class SpringIntegrationTest extends BaseTest {
 }
