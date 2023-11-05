2eb2c0bcf33f6cf9fcf1f2d5ce9b77d4f5790059
Abul Syed <syed1234@hotmail.co.uk>
2023-11-05 15:09:10 +0000

    update getUser exception

--- identity-service/src/main/java/com/syed/identityservice/service/impl/UserServiceImpl.java
+++ identity-service/src/main/java/com/syed/identityservice/service/impl/UserServiceImpl.java
@@ -48,6 +48,10 @@ public class UserServiceImpl implements UserService {
                     new ResourceNotFoundException(ErrorConstant.RESOURCE_NOT_FOUND.formatMessage("User with id " + userId)));
         } else if (appName == null && username != null) {
             user = userRepository.findByUsername(username);
+
+            if (user == null) {
+                throw new ResourceNotFoundException(ErrorConstant.RESOURCE_NOT_FOUND.formatMessage("Username " + username));
+            }
         } else if (appName != null && username != null) {
             AppEntity app = appRepository.findByName(appName);
 
