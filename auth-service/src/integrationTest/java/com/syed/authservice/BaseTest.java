1a4adf079a6789246851914d02418b1c52b05e4e
Abul Syed <syed1234@hotmail.co.uk>
2023-11-28 21:04:10 +0000

    wiremock integration test

---
+++ auth-service/src/integrationTest/java/com/syed/authservice/BaseTest.java
@@ -0,0 +1,22 @@
+package com.syed.authservice;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class BaseTest {
+
+    @BeforeEach
+    void setup() {
+        // Manually initialize & configure WireMock it to use the correct resource path
+        WireMockServer wireMockServer = new WireMockServer(
+                WireMockConfiguration.wireMockConfig()
+                        .port(8081)
+                        .usingFilesUnderDirectory("src/integrationTest/resources")
+        );
+
+        wireMockServer.start();
+        WireMock.configureFor("localhost", wireMockServer.port());
+    }
+}
---
+++ auth-service/src/integrationTest/java/com/syed/authservice/GetClientIntegrationTest.java
@@ -0,0 +1,24 @@
+package com.syed.authservice;
+
+import com.syed.authservice.clients.IdentityServiceClient;
+import com.syed.authservice.domain.model.response.ClientResponse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+public class GetClientIntegrationTest extends BaseTest {
+
+    @Autowired
+    private IdentityServiceClient identityServiceClient;
+
+    @Test
+    void getClient() {
+        ResponseEntity<ClientResponse> res = identityServiceClient.getClient("auth-service", "test", "client");
+
+        assertEquals(HttpStatus.OK, res.getStatusCode());
+    }
+}
