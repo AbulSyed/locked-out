90c3e459a5efb238d29c7b3fb7edaecbe816da0a
Abul Syed <syed1234@hotmail.co.uk>
2023-12-09 23:36:32 +0000

    update consumer expected contract

--- auth-service/src/contractTest/java/com/syed/authservice/consumer/IdentityServiceConsumerTest.java
+++ auth-service/src/contractTest/java/com/syed/authservice/consumer/IdentityServiceConsumerTest.java
@@ -10,7 +10,6 @@ import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.*;
 import au.com.dius.pact.core.model.annotations.Pact;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syed.authservice.domain.model.response.ClientResponse;
 import org.junit.jupiter.api.Test;
@@ -23,9 +22,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
-import static au.com.dius.pact.consumer.dsl.LambdaDsl.newJsonArray;
+import static au.com.dius.pact.consumer.dsl.LambdaDsl.newJsonBody;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 // https://docs.pact.io/implementation_guides/jvm/consumer/junit5
@@ -43,7 +40,7 @@ class IdentityServiceConsumerTest {
     public V4Pact getClientPact(PactDslWithProvider builder) {
         return builder
                 .given("Get client by appName & clientId")
-                .uponReceiving("A requests for a client by appName & clientId")
+                .uponReceiving("A request for a client by appName & clientId")
                 .path("/get-client")
                 .query("appName=myapp&clientId=client1")
                 .headers("x-correlation-id", "123")
@@ -69,23 +66,25 @@ class IdentityServiceConsumerTest {
 
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
-        List<ClientResponse> clientResponseList = mapper.readValue(response.getBody(), new TypeReference<List<ClientResponse>>() {});
+        ClientResponse clientResponseList = mapper.readValue(response.getBody(), ClientResponse.class);
 
         assertNotNull(clientResponseList);
     }
 
+    // docs: https://docs.pact.io/implementation_guides/jvm/consumer#building-json-bodies-with-pactdsljsonbody-dsl
+    // examples: https://docs.pact.io/implementation_guides/jvm/consumer#examples
     private DslPart buildGetClientPact() {
-        return newJsonArray(o -> o.object(ob -> ob
-                .numberType("id", 1)
-                .stringType("clientId", "client1")
-                .stringType("clientSecret", "secret")
-                .array("roles", a -> {})
-                .array("authorities", a -> {})
-                .array("scopes", a -> {})
-                .array("authMethod", a -> {})
-                .array("authGrantType", a -> {})
-                .stringType("redirectUri", "http://127.0.0.1:3000/authorized")
-                .stringType("createdAt", "2023-11-30T12:26:55.732213")
-        )).build();
+        return newJsonBody((o) -> {
+            o.numberValue("id", 1);
+            o.stringValue("clientId", "client1");
+            o.stringValue("clientSecret", "secret");
+            o.array("roles", a -> {});
+            o.array("authorities", a -> {});
+            o.array("scopes", a -> {});
+            o.array("authMethod", a -> {});
+            o.array("authGrantType", a -> {});
+            o.stringValue("redirectUri", "http://127.0.0.1:3000/authorized");
+            o.stringValue("createdAt", "2023-11-30T12:26:55.732213");
+        }).build();
     }
 }
