7e5e20002a20de50e0e0a8193d5fde7eff2ddb2f
Abul Syed <syed1234@hotmail.co.uk>
2023-12-14 21:13:24 +0000

    add new contract getUserPact

--- auth-service/src/contractTest/java/com/syed/authservice/consumer/IdentityServiceConsumerTest.java
+++ auth-service/src/contractTest/java/com/syed/authservice/consumer/IdentityServiceConsumerTest.java
@@ -12,6 +12,7 @@ import au.com.dius.pact.core.model.annotations.Pact;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syed.authservice.domain.model.response.ClientResponse;
+import com.syed.authservice.domain.model.response.UserV2Response;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,10 +52,24 @@ class IdentityServiceConsumerTest {
                 .toPact(V4Pact.class);
     }
 
-    @Test
-    void test(MockServer mockServer) throws JsonProcessingException {
-        System.out.println(mockServer.getUrl());
+    @Pact(provider = "lockedOut_identityService", consumer = "lockedOut_authService")
+    public V4Pact getUserPact(PactDslWithProvider builder) {
+        return builder
+                .given("Get user by username")
+                .uponReceiving("A request for a user by username")
+                .path("/get-user")
+                .query("username=admin")
+                .headers("x-correlation-id", "123")
+                .method("GET")
+                .willRespondWith()
+                .status(200)
+                .body(buildGetUserPact())
+                .toPact(V4Pact.class);
+    }
 
+    @Test
+    @PactTestFor(pactMethod = "getClientPact")
+    void getClientTest(MockServer mockServer) throws JsonProcessingException {
         String url = mockServer.getUrl() + "/get-client?appName=myapp&clientId=client1";
 
         RestTemplate restTemplate = new RestTemplate();
@@ -66,9 +81,28 @@ class IdentityServiceConsumerTest {
 
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
 
-        ClientResponse clientResponseList = mapper.readValue(response.getBody(), ClientResponse.class);
+        ClientResponse clientResponse = mapper.readValue(response.getBody(), ClientResponse.class);
 
-        assertNotNull(clientResponseList);
+        assertNotNull(clientResponse);
+    }
+
+    @Test
+    @PactTestFor(pactMethod = "getUserPact")
+    void getUserTest(MockServer mockServer) throws JsonProcessingException {
+        String url = mockServer.getUrl() + "/get-user?username=admin";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-correlation-id", "123");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+
+        UserV2Response userV2Response = mapper.readValue(response.getBody(), UserV2Response.class);
+
+        assertNotNull(userV2Response);
     }
 
     // docs: https://docs.pact.io/implementation_guides/jvm/consumer#building-json-bodies-with-pactdsljsonbody-dsl
@@ -84,6 +118,19 @@ class IdentityServiceConsumerTest {
             o.array("authMethod", a -> {});
             o.array("authGrantType", a -> {});
             o.stringType("redirectUri", "http://127.0.0.1:3000/authorized");
+            o.stringType("createdAt", "2023-11-30T12:26:55.732213");
+        }).build();
+    }
+
+    private DslPart buildGetUserPact() {
+        return newJsonBody((o) -> {
+            o.numberType("id", 1);
+            o.stringType("username", "admin");
+            o.stringType("password", "admin");
+            o.stringType("email", "a@m.com");
+            o.stringType("phoneNumber", "07123123123");
+            o.array("roles", a -> {});
+            o.array("authorities", a -> {});
             o.stringType("createdAt", "2023-11-30T12:26:55.732213");
         }).build();
     }
