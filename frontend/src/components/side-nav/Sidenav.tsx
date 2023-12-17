1793c17eaeb6fb7a96ed31e9ffe4210146a344f2
Abul Syed <syed1234@hotmail.co.uk>
2023-12-17 20:08:14 +0000

    show active app

--- frontend/src/components/side-nav/Sidenav.tsx
+++ frontend/src/components/side-nav/Sidenav.tsx
@@ -1,19 +1,26 @@
 import './Sidenav.scss'
 
-import { NavLink } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom'
+import { useAppSelector } from '../../store/hooks'
 
 interface SidenavProps {
   route: string
 }
 
 const Sidenav: React.FC<SidenavProps> = ({ route }) => {
+  const state = useAppSelector(state => state.app)
+  const location = useLocation()
+  const activeApp = location.pathname.split('/')[2]
+
   return (
     <div className='sidenav'>
       <ul className='sidenav-list'>
         <select className="app-select">
-          <option value="app-1">app-1</option>
-          <option value="app-2">app-2</option>
-          <option value="app-3">app-3</option>
+          {
+            state.apps.map(app => (
+              <option key={app.id} value={app.name} selected={app.name == activeApp}>{app.name}</option>
+            ))
+          }
         </select>
         <hr className="sidenav-hr"/>
         <NavLink className={({ isActive, isPending }) => isPending ? "pending li" : isActive ? "active li" : "li"} to={route + "/overview"}>
