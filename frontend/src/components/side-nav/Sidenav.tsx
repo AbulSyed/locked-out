a97cee80d635417f9504b0e63a0b2f251ee426aa
Abul Syed <syed1234@hotmail.co.uk>
2024-01-07 21:31:46 +0000

    handleSelectChange

--- frontend/src/components/side-nav/Sidenav.tsx
+++ frontend/src/components/side-nav/Sidenav.tsx
@@ -1,9 +1,12 @@
 import './Sidenav.scss'
 
 import { useEffect } from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 import { useAppSelector, useAppDispatch } from '../../store/hooks'
 import { setActiveNavReducer } from '../../store/active-nav/activeNavSlice'
+import { getAppDetails } from '../../store/app/appSlice'
+import { getUsersByAppName } from '../../store/user/userSlice'
+import { getClientsByAppName } from '../../store/client/clientSlice'
 
 interface SidenavProps {
 }
@@ -17,6 +20,8 @@ const Sidenav: React.FC<SidenavProps> = () => {
   const dispatch = useAppDispatch()
   const activeApp = location.pathname.split('/')[2]
 
+  const navigate = useNavigate()
+
   useEffect(() => {
     dispatch(setActiveNavReducer('Overview'))
   }, [])
@@ -25,10 +30,33 @@ const Sidenav: React.FC<SidenavProps> = () => {
     dispatch(setActiveNavReducer(navItem))
   }
 
+  const handleSelectChange = (e: any) => {
+    const app = e.target.value
+
+    navigate(`/apps/${app}/overview`)
+
+    dispatch(setActiveNavReducer('Overview'))
+
+    dispatch(getAppDetails(app))
+    dispatch(getUsersByAppName({
+      "appName": app,
+      "page": "1",
+      "size": "100"
+    }))
+    dispatch(getClientsByAppName({
+      "appName": app,
+      "page": "1",
+      "size": "100"
+    }))
+  }
+
   return (
     <div className='sidenav'>
       <div className='sidenav-list'>
-        <select className='app-select'>
+        <select
+          className='app-select'
+          onChange={handleSelectChange}
+        >
           {
             state.apps.map(app => (
               <option
