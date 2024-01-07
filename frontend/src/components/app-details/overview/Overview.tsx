7ae49544ed73e0216805996b506095741f81db90
Abul Syed <syed1234@hotmail.co.uk>
2024-01-07 18:47:47 +0000

    overview section

--- frontend/src/components/app-details/overview/Overview.tsx
+++ frontend/src/components/app-details/overview/Overview.tsx
@@ -1,6 +1,7 @@
 import './Overview.scss'
 
 import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../../../store/hooks';
 
 interface OverviewProps {
 }
@@ -8,9 +9,25 @@ interface OverviewProps {
 const Overview: React.FC<OverviewProps> = () => {
   const { appname } = useParams()
 
+  const appDetails = useAppSelector(state => state.app.appDetails)
+
   return ( 
     <div className='overview'>
       <p className='m-heading py-2'>App: {appname}</p>
+      {
+        appDetails.users.length == 0 ? (
+          <p>No users registered</p>
+        ) : appDetails.users.length == 1 ? (
+          <p>{appDetails.users.length} user registered with app</p>
+        ) : <p>{appDetails.users.length} users registered with app</p>
+      }
+      {
+        appDetails.clients.length == 0 ? (
+          <p>No clients registered</p>
+        ) : appDetails.clients.length == 1 ? (
+          <p>{appDetails.clients.length} client registered with app</p>
+        ) : <p>{appDetails.clients.length} clients registered with app</p>
+      }
     </div>
   );
 }
