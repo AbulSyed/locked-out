49b5534ca7308fd818368522f68b626cb7475cc6
Abul Syed <syed1234@hotmail.co.uk>
2026-04-18 22:36:33 +0100

    bootstrap s3 w/ versioning, encryption & block public access

---
+++ infra/bootstrap-backend.tf
@@ -0,0 +1,35 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "locked-out-terraform-state-588738611864"
+
+  tags = {
+    Name        = "Locked Out Terraform State Bucket"
+    Environment = "shared"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state_block_public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
