69d8aa5155d9b2969fce1da142ac41d1acb9aad3
Abul Syed <syed1234@hotmail.co.uk>
2026-04-19 10:42:33 +0100

    create ecr repos

---
+++ infra/modules/ecr/main.tf
@@ -0,0 +1,14 @@
+resource "aws_ecr_repository" "this" {
+  name = var.name
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  force_delete = true
+
+  tags = {
+    Service     = var.service
+    Environment = var.env
+  }
+}
