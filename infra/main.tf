aba615daef6af58da5cbe9e5bc1b8a7800a001e5
Abul Syed <syed1234@hotmail.co.uk>
2026-04-29 21:43:12 +0100

    rename ECR registries

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,20 @@
 module "auth_ecr" {
   source  = "./modules/ecr"
-  name    = "auth-service"
+  name    = "locked-out-auth-service"
   service = "auth"
   env     = "dev"
 }
 
 module "identity_ecr" {
   source  = "./modules/ecr"
-  name    = "identity-service"
+  name    = "locked-out-identity-service"
   service = "identity"
   env     = "dev"
 }
 
 module "frontend_ecr" {
   source  = "./modules/ecr"
-  name    = "frontend-service"
+  name    = "locked-out-frontend-service"
   service = "frontend"
   env     = "dev"
 }
