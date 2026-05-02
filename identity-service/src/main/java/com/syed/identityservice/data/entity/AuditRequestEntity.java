10ce8590b255c36eb62973a4c8cabbe9934ec49e
Abul Syed <syed1234@hotmail.co.uk>
2026-05-02 16:11:36 +0100

    fix ecr tagging bug

--- identity-service/src/main/java/com/syed/identityservice/data/entity/AuditRequestEntity.java
+++ identity-service/src/main/java/com/syed/identityservice/data/entity/AuditRequestEntity.java
@@ -3,7 +3,14 @@ package com.syed.identityservice.data.entity;
 import com.syed.identityservice.domain.enums.ProcessEnum;
 import com.syed.identityservice.domain.enums.RequestStatusEnum;
 import com.syed.identityservice.domain.enums.RequestTypeEnum;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 import java.time.LocalDateTime;
