3d910163c8a73d6f48b68d5902bdad3c725670bf
Abul Syed <syed1234@hotmail.co.uk>
2026-05-18 21:19:24 +0100

    s3 module

--- infra/main.tf
+++ infra/main.tf
@@ -7,7 +7,7 @@ module "ecr" {
 }
 
 module "vpc" {
-  source = "git::https://github.com/AbulSyed/aws-terraform-modules.git//vpc?ref=vpc"
+  source = "git::https://github.com/AbulSyed/aws-terraform-modules.git//vpc"
 
   vpc_name                  = "My VPC"
   vpc_cidr                  = "10.0.0.0/16"
@@ -20,4 +20,11 @@ module "vpc" {
   nat_gateway_name          = "My NAT GW"
   private_rt_destination_ip = "0.0.0.0/0"
   private_rt_name           = "My Private Route Table"
+}
+
+module "vpc" {
+  source = "git::https://github.com/AbulSyed/aws-terraform-modules.git//s3"
+
+  bucket = "state-bucket-123"
+  bucket_name = "Terraform State Bucket"
 }
